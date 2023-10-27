f83922a917c3d076599a534c95e14dbee38d008b
David <145173380+D4V1D13AFK@users.noreply.github.com>
2023-10-26 21:39:51 -0600

    Add files via upload

--- main_code.ino
+++ main_code.ino
@@ -25,8 +25,8 @@ cintura_2.attach(10);
 pata_3.attach(9);
 cintura_3.attach(8);
 
-pata_4.attach(7);
-cintura_4.attach(6);
+pata_4.attach(6);
+cintura_4.attach(7);
 
 pata_5.attach(7);
 cintura_5.attach(6);
@@ -53,16 +53,16 @@ void loop() {
 
 void base(){
   pata_1.write(90);
-  cintura_1.write(80);
+  cintura_1.write(30);
 
   pata_2.write(0);
   cintura_2.write(100);
 
   pata_3.write(90);
-  cintura_3.write(80);
+  cintura_3.write(90);
 
-  pata_4.write(90);
-  cintura_4.write(120);
+  pata_4.write(0);
+  cintura_4.write(110);
 
   pata_5.write(90);
   cintura_5.write(125);
@@ -72,34 +72,36 @@ void base(){
 }
 
 void adelante_1(){
-  pata_1.write(0);
-  pata_4.write(180);
+  
+  pata_1.write(30);
+  pata_4.write(60);
   pata_5.write(35);
-  delay(500);
-  cintura_1.write(50);
-  cintura_4.write(150);
+  delay(200);
+
+  cintura_1.write(60);
+  cintura_4.write(110);
   cintura_5.write(85);
-  delay(500);
+  delay(200);
+
   pata_1.write(90);
-  pata_4.write(90);
+  pata_4.write(0);
   pata_5.write(125);
-  delay(500);
+  delay(200);
 }
 
 void adelante_2(){
-  pata_2.write(90);
-  pata_3.write(0);
+  pata_2.write(60);
+  pata_3.write(10);
   pata_6.write(125);
-  delay(500);
-  cintura_2.write(130);
-  cintura_3.write(50);
+  delay(200);
+
+  cintura_2.write(80);
+  cintura_3.write(120);
   cintura_6.write(100);
-  delay(500);
+  delay(200);
+
   pata_2.write(0);
   pata_3.write(90);
   pata_6.write(25);
-  delay(500);
+  delay(200);
 }
-
- 
-
